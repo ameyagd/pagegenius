08c2c1ca62a33369956a0227aaff2cca11ec2514
ameya.g <ameya.g@media.net>
2025-03-11 19:22:43 +0530

    fix: removed unwanted files

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { AuthProvider } from './contexts/AuthContext'
+import { AuthProvider } from './components/LandingPageManager/contexts/AuthContext.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
